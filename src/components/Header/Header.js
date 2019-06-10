436ffef2e043979c97d73d5b927c544104992751
Navin Kumar <navin1551@gmail.com>
2019-06-10 10:01:24 -0700

    pushing to repo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,56 +1,51 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Hyph } from '../Utils/Utils'
-import TokenService from '../../services/token-service'
-import './Header.css'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Hyph } from "../Utils/Utils";
+import TokenService from "../../services/token-service";
+import IdleService from "../../services/idle-service";
+import "./Header.css";
 
 export default class Header extends Component {
   handleLogoutClick = () => {
-  }
+    TokenService.clearAuthToken();
+    /* when logging out, clear the callbacks to the refresh api and idle auto logout */
+    TokenService.clearCallbackBeforeExpiry();
+    IdleService.unRegisterIdleResets();
+  };
 
   renderLogoutLink() {
     return (
-      <div className='Header__logged-in'>
-        <Link
-          onClick={this.handleLogoutClick}
-          to='/'>
+      <div className="Header__logged-in">
+        <Link onClick={this.handleLogoutClick} to="/">
           Logout
         </Link>
       </div>
-    )
+    );
   }
 
   renderLoginLink() {
     return (
-      <div className='Header__not-logged-in'>
-        <Link
-          to='/register'>
-          Register
-        </Link>
+      <div className="Header__not-logged-in">
+        <Link to="/register">Register</Link>
         <Hyph />
-        <Link
-          to='/login'>
-          Log in
-        </Link>
+        <Link to="/login">Log in</Link>
       </div>
-    )
+    );
   }
 
   render() {
     return (
-      <nav className='Header'>
+      <nav className="Header">
         <h1>
-          <Link to='/'>
-            <FontAwesomeIcon className='green' icon='frog' />
-            {' '}
-            Blogful Client
+          <Link to="/">
+            <FontAwesomeIcon className="green" icon="frog" /> Blogful Client
           </Link>
         </h1>
         {TokenService.hasAuthToken()
           ? this.renderLogoutLink()
           : this.renderLoginLink()}
       </nav>
-    )
+    );
   }
 }
